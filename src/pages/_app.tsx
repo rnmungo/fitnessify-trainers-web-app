047831d2c8414751827230962be21e88ab7caf6c
Rodrigo Mungo <rodrigomungo@gmail.com>
2025-01-08 00:33:21 -0300

    feat: :sparkles: add root pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+
+import ThemeProvider from '@/core/context/theme';
+import SnackbarProvider from '@/core/context/snackbar';
+import QueryProvider from '@/core/context/query';
+import SessionProvider from '@/core/auth/context/session';
+import { DEFAULT_CONFIGURATION } from '@/constants/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryProvider>
+      <SessionProvider>
+        <ThemeProvider theme={DEFAULT_CONFIGURATION}>
+          <SnackbarProvider>
+            <Component {...pageProps} />
+          </SnackbarProvider>
+        </ThemeProvider>
+      </SessionProvider>
+    </QueryProvider>
+  );
 }
