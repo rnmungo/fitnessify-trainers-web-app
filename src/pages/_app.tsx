4e27cecdadf3831c6f4195096c5a2125807f2634
Rodrigo Mungo <rodrigomungo@gmail.com>
2025-01-20 18:43:44 -0300

    feat: :sparkles: refactor app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,22 @@
-import type { AppProps } from 'next/app';
-
 import ThemeProvider from '@/core/context/theme';
 import SnackbarProvider from '@/core/context/snackbar';
 import QueryProvider from '@/core/context/query';
 import SessionProvider from '@/core/auth/context/session';
 import { DEFAULT_CONFIGURATION } from '@/constants/theme';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <QueryProvider>
-      <SessionProvider>
-        <ThemeProvider theme={DEFAULT_CONFIGURATION}>
-          <SnackbarProvider>
-            <Component {...pageProps} />
-          </SnackbarProvider>
-        </ThemeProvider>
-      </SessionProvider>
-    </QueryProvider>
-  );
-}
+import type { AppProps } from 'next/app';
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <QueryProvider>
+    <SessionProvider>
+      <ThemeProvider theme={DEFAULT_CONFIGURATION}>
+        <SnackbarProvider>
+          <Component {...pageProps} />
+        </SnackbarProvider>
+      </ThemeProvider>
+    </SessionProvider>
+  </QueryProvider>
+);
+
+
+export default App;
